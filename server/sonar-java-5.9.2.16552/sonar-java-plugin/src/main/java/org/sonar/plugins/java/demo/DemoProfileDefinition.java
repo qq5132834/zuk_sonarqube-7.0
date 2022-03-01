fcaa30e4d108b9e38e476fa403172f3624b05b53
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-03-01 11:23:55 +0800

    java-plugin is ok

--- server/sonar-java-5.9.2.16552/sonar-java-plugin/src/main/java/org/sonar/plugins/java/demo/DemoProfileDefinition.java
+++ server/sonar-java-5.9.2.16552/sonar-java-plugin/src/main/java/org/sonar/plugins/java/demo/DemoProfileDefinition.java
@@ -4,6 +4,9 @@ import org.sonar.api.profiles.ProfileDefinition;
 import org.sonar.api.profiles.RulesProfile;
 import org.sonar.api.utils.ValidationMessages;
 
+/***
+ * 暂时不要，因为server启动可能报错
+ */
 public class DemoProfileDefinition extends ProfileDefinition {
     @Override
     public RulesProfile createProfile(ValidationMessages validation) {
